09f4715ebc065ffcbb9eea523e525ac21a2b4f6d
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2026-03-14 18:00:57 +0300

    test: add e2e tests to create chat api

---
+++ app/tests/application/api/test_messages.py
@@ -0,0 +1,58 @@
+import pytest
+
+from httpx import Response
+
+from faker import Faker
+
+from fastapi.testclient import TestClient
+from fastapi import FastAPI, status
+
+
+@pytest.mark.asyncio
+async def test_create_chat_success(
+    app: FastAPI,
+    client: TestClient,
+    faker: Faker,
+):
+    url = app.url_path_for('create_chat_handler')
+    title: str = faker.text(max_nb_chars=30)
+    response: Response = client.post(url=url, json={'title': title})
+
+    assert response.is_success
+
+    json_data = response.json()
+
+    assert json_data['title'] == title
+
+
+@pytest.mark.asyncio
+async def test_create_chat_fail_title_text_too_long(
+    app: FastAPI,
+    client: TestClient,
+    faker: Faker,
+):
+    url = app.url_path_for('create_chat_handler')
+    title: str = faker.text(max_nb_chars=500)
+    response: Response = client.post(url=url, json={'title': title})
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST, response.json()
+
+    json_data = response.json()
+
+    assert json_data['detail']['error']
+
+
+@pytest.mark.asyncio
+async def test_create_chat_fail_title_text_is_empty(
+    app: FastAPI,
+    client: TestClient,
+    faker: Faker,
+):
+    url = app.url_path_for('create_chat_handler')
+    response: Response = client.post(url=url, json={'title': ''})
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST, response.json()
+
+    json_data = response.json()
+
+    assert json_data['detail']['error']
