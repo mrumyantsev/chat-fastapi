b3970e4b2fc3e7eaa200794ee6e381f630874cdc
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2026-03-14 15:45:02 +0300

    feat: add endpoint to create chat and fixes

--- app/application/api/main.py
+++ app/application/api/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 
+from application.api.messages.handlers import router as message_router
 
-def create_app():
-    return FastAPI(
+
+def create_app() -> FastAPI:
+    app = FastAPI(
         title='Simple Kafka Chat',
         docs_url='/api/docs',
         description='Simple Kafka + DDD Example',
         debug=True,
     )
+
+    app.include_router(prefix='/chat', router=message_router)
+
+    return app
---
+++ app/application/api/messages/handlers.py
@@ -0,0 +1,39 @@
+from fastapi import Depends, status
+from fastapi.exceptions import HTTPException
+from fastapi.routing import APIRouter
+
+from application.api.messages.schemas import CreateChatRequestSchema, CreateChatResponseSchema
+from application.api.schemas import ErrorSchema
+from domain.exceptions.base import ApplicationException
+from logic.commands.messages import CreateChatCommand
+from logic.init import init_container
+from logic.mediator import Mediator
+
+
+router = APIRouter(tags=['Chat'])
+
+
+@router.post(
+    '',
+    response_model=CreateChatResponseSchema,
+    status_code=status.HTTP_201_CREATED,
+    description=(
+        'Endpoint that creates new chat. If the chat with such name exists, '
+        'returns 400 status code.'
+    ),
+    responses={
+        status.HTTP_201_CREATED: {'model': CreateChatResponseSchema},
+        status.HTTP_400_BAD_REQUEST: {'model': ErrorSchema},
+    },
+)
+async def create_chat_handler(schema: CreateChatRequestSchema, container=Depends(init_container)):
+    '''Creates new chat.'''
+
+    mediator: Mediator = container.resolve(Mediator)
+
+    try:
+        chat, *_ = await mediator.handle_command(CreateChatCommand(title=schema.title))
+    except ApplicationException as exception:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail={'error': exception.message})
+
+    return CreateChatResponseSchema.from_entity(chat=chat)
